38b7d3549f262d662a5f0d3389b9b0c7a98e42f3
Brane <braneopacic@gmail.com>
2018-07-02 22:00:51 +0200

    Minor changes to InfoRow

--- src/Components/InfoRow/InfoRow.js
+++ src/Components/InfoRow/InfoRow.js
@@ -5,28 +5,41 @@ const InfoRow = (props) => {
 
     console.log("From info row")
     console.log(props.info.login);
-    
+
 
     switch (props.type) {
         case 'name':
             return <div className={css.InfoRow}>
                 <span className={css.InfoLabel}>Name</span>
-                <span className={css.InfoText}>{props.info.login}</span>
+                <div className={css.rightSection}>
+                    <span className={css.InfoText}>{props.info.login}</span>
+                    <span className={css.changeText}>+</span>
+                </div>
             </div>
         case 'blog':
             return <div className={css.InfoRow}>
                 <span className={css.InfoLabel}>Blog</span>
-                <span className={css.InfoText}>{props.info.blog}</span>
+                <div className={css.rightSection}>
+                    <span className={css.InfoText}>{props.info.blog}</span>
+                    <span className={css.changeText}>+</span>
+                </div>
             </div>
         case 'company':
             return <div className={css.InfoRow}>
                 <span className={css.InfoLabel}>Company</span>
-                <span className={css.InfoText}>{props.info.company}</span>
+                <div className={css.rightSection}>
+                    <span className={css.InfoText}>{props.info.company}</span>
+                    <span className={css.changeText}>+</span>
+                </div>
             </div>
         case 'location':
             return <div className={css.InfoRow}>
                 <span className={css.InfoLabel}>Location</span>
-                <span className={css.InfoText}>{props.info.location}</span>
+                <div className={css.rightSection}>
+                    <span className={css.InfoText}>{props.info.location}</span>
+                    <span className={css.changeText}>+</span>
+                </div>
+
             </div>
         default:
             return null;
